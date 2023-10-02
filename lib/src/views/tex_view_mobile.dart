16cc9b72c0b6b265260d071e2e70148666e0c630
Can Arslan <canarslanx@gmail.com>
2023-10-02 19:23:09 +0300

    new file:   .flutter-plugins 	new file:   .flutter-plugins-dependencies 	modified:   example/.flutter-plugins-dependencies 	modified:   example/pubspec.lock 	modified:   lib/src/views/tex_view_mobile.dart

--- lib/src/views/tex_view_mobile.dart
+++ lib/src/views/tex_view_mobile.dart
@@ -3,14 +3,82 @@ import 'package:flutter_tex/flutter_tex.dart';
 import 'package:flutter_tex/src/utils/core_utils.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+// #enddocregion platform_imports
 class TeXViewState extends State<TeXView> with AutomaticKeepAliveClientMixin {
-  WebViewController? _controller;
+  late final WebViewController _controller;
 
   double _height = minHeight;
   String? _lastData;
   bool _pageLoaded = false;
 
+  late final PlatformWebViewControllerCreationParams params;
   @override
+  void initState() {
+    super.initState();
+
+    // #docregion platform_features
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.transparent)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (message) {
+            _pageLoaded = true;
+            _initTeXView();
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..addJavaScriptChannel('TeXViewRenderedCallback',
+          onMessageReceived: (jm) async {
+        double height = double.parse(jm.message);
+        if (_height != height) {
+          setState(() {
+            _height = height;
+          });
+        }
+        widget.onRenderFinished?.call(height);
+      })
+      ..addJavaScriptChannel('OnTapCallback', onMessageReceived: (jm) {
+        widget.child.onTapCallback(jm.message);
+      })
+      ..loadRequest(Uri.parse(
+          "packages/flutter_tex/js/${widget.renderingEngine?.name ?? 'katex'}/index.html"));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;
+  }
+
   bool get wantKeepAlive => true;
 
   @override
@@ -26,41 +94,7 @@ class TeXViewState extends State<TeXView> with AutomaticKeepAliveClientMixin {
           : 0,
       children: <Widget>[
         SizedBox(
-          height: _height,
-          child: WebView(
-            onPageFinished: (message) {
-              _pageLoaded = true;
-              _initTeXView();
-            },
-            initialUrl:
-                "packages/flutter_tex/js/${widget.renderingEngine?.name ?? 'katex'}/index.html",
-            onWebViewCreated: (controller) {
-              _controller = controller;
-            },
-            initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
-            backgroundColor: Colors.transparent,
-            allowsInlineMediaPlayback: true,
-            javascriptChannels: {
-              JavascriptChannel(
-                  name: 'TeXViewRenderedCallback',
-                  onMessageReceived: (jm) async {
-                    double height = double.parse(jm.message);
-                    if (_height != height) {
-                      setState(() {
-                        _height = height;
-                      });
-                    }
-                    widget.onRenderFinished?.call(height);
-                  }),
-              JavascriptChannel(
-                  name: 'OnTapCallback',
-                  onMessageReceived: (jm) {
-                    widget.child.onTapCallback(jm.message);
-                  })
-            },
-            javascriptMode: JavascriptMode.unrestricted,
-          ),
-        ),
+            height: _height, child: WebViewWidget(controller: _controller)),
         widget.loadingWidgetBuilder?.call(context) ?? const SizedBox.shrink()
       ],
     );
@@ -69,7 +103,7 @@ class TeXViewState extends State<TeXView> with AutomaticKeepAliveClientMixin {
   void _initTeXView() {
     if (_pageLoaded && _controller != null && getRawData(widget) != _lastData) {
       if (widget.loadingWidgetBuilder != null) _height = minHeight;
-      _controller!.runJavascript("initView(${getRawData(widget)})");
+      _controller.runJavaScript("initView(${getRawData(widget)})");
       _lastData = getRawData(widget);
     }
   }
